e81b7e7f75ad410e0f6c6aa4868166ba6e94071d
vidhirkaup <vidhir.kaup@gmail.com>
2021-05-02 18:12:50 -0400

    get single team detail

---
+++ src/main/java/com/vlabs/eco/games/controller/TeamController.java
@@ -0,0 +1,25 @@
+package com.vlabs.eco.games.controller;
+
+import com.vlabs.eco.games.domain.Team;
+import com.vlabs.eco.games.repository.TeamRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class TeamController {
+
+    private TeamRepository teamRepository;
+
+    public TeamController(TeamRepository teamRepository) {
+        this.teamRepository = teamRepository;
+    }
+
+    @GetMapping("/teams/{teamName}")
+    public Team getTeam(@PathVariable String teamName){
+        return teamRepository.findByTeamName(teamName);
+    }
+
+}
