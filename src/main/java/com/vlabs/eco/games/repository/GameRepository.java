3cf7ce0f66a72f24cebfd96d8d76872c3d895ccf
vidhirkaup <vidhir.kaup@gmail.com>
2021-05-15 16:16:03 -0400

    replaced JPA method with Named Query

--- src/main/java/com/vlabs/eco/games/repository/GameRepository.java
+++ src/main/java/com/vlabs/eco/games/repository/GameRepository.java
@@ -3,7 +3,9 @@ package com.vlabs.eco.games.repository;
 import com.vlabs.eco.games.domain.Game;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -21,9 +23,9 @@ public interface GameRepository extends CrudRepository<Game, Long> {
         return getByTeam1OrTeam2OrderByDateDesc(teamName, teamName, PageRequest.of(0, count));
     }
 
-    default List<Game> findGamesByYear(String teamName, LocalDate date1, LocalDate date2) {
-        return getByTeam1AndDateBetweenOrTeam2AndDateBetweenOrderByDateDesc(teamName, date1, date2,
-                                                                            teamName, date1, date2);
-    }
+    @Query("select g from Game g where (g.team1 = :teamName or g.team2 = :teamName) and (g.date between :date1 and :date2)" )
+    List<Game> findGamesByYear(@Param("teamName") String teamName,
+                               @Param("date1") LocalDate date1,
+                               @Param("date2") LocalDate date2);
 
 }
